d53815145310c30bb9c40bbbf4c73f232a0b4958
vedanshisharma <vedanshisharma12@gmail.com>
2025-04-05 14:42:42 +0530

    added property reviews api

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ const OfferRoutes=require("./src/routes/offerRoutes")
 const propertyRoutes=require("./src/routes/PropertyRoutes")
 const propertiesFilter=require("./src/routes/propertFiletrRoutes")
 const { errorHandler } = require("./src/middleware/errorHandler");
+const chatBotRoutes=require("./src/routes/ChatBotRoutes");
+const adsRouters=require("./src/routes/adsRoutes")
+const propertyReviewRoutes=require("./src/routes/propertyReviewRoutes")
 
 
 // app.set("trust proxy", 1); // 👈 Fix for AWS/Nginx
@@ -49,7 +52,7 @@ app.use("/api/v1/chatbot",chatBotRoutes)
 app.use("/api/banners",BannerRoutes)
 app.use("/api/offers",OfferRoutes)
 app.use("/api/ads",adsRouters)
-
+app.use("/api/review",propertyReviewRoutes)
 
 
 // Global Error Handler (Must be at the bottom)
